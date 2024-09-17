375b723d57624efb6a9df4fca47465bf0ad6917f
Will Trimble <nospam@nowhere.org>
2024-09-17 11:46:43 -0500

    removed project stub

--- attendancechimp/app/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Course(models.Model):
-    name = models.CharField(max_length=256, primary_key=True)
-    number = models.CharField(max_length=5)
--- attendancechimp/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('handleform', views.handle_form, name='form'),
-    path('', views.index, name='index'),
-    #path('handleform', views.handle_form, name='form'),
-]
