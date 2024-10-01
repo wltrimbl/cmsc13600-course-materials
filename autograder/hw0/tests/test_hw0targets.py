342c0682c00be048065e48b886b4f2c58a92aebb
Will Trimble <nospam@nowhere.org>
2024-10-01 10:44:17 -0500

    Updates to hw0 autograder, 4 points

--- autograder/hw0/tests/test_hw0targets.py
+++ autograder/hw0/tests/test_hw0targets.py
@@ -9,20 +9,22 @@ from gradescope_utils.autograder_utils.decorators import weight, number
 
 if platform.system() == "Darwin":
     MD5 = "md5"
+    AG = "./"
 else:
     MD5 = "md5sum"
+    AG = "/autograder/submission/"
 
 
 class TestHelloWorld(unittest.TestCase):
-    '''Test helloworld, cat, and existence of scavenger.txt'''
+    '''Test targets for hw0: names.txt, nobel-prize-laureates.csv, unneded_data.csv'''
     def setUp(self):
         pass
 
     @weight(1)
     @number("1.0")
     def test_names_txt_exists(self):
-        '''Test helloworld.py exists'''
-        self.assertTrue(os.path.exists("names.txt"),
+        '''Test names.txt exists'''
+        self.assertTrue(os.path.exists(AG+"names.txt"),
                         "names.txt does not exist!")
         file_size = os.path.getsize('names.txt')
         self.assertGreater(file_size, 5, "names.txt is too small")
@@ -31,14 +33,14 @@ class TestHelloWorld(unittest.TestCase):
     @number("2.0")
     def test_unneeded_data_absent(self):
         '''Test unneeded_data.csv does not exist'''
-        self.assertFalse(os.path.exists("unneeded_data.csv"),
+        self.assertFalse(os.path.exists(AG+"unneeded_data.csv"),
                         "unneeded_data.csv exists!")
 
     @weight(1)
     @number("3.0")
     def test_nobel(self):
         '''Check size and length of nobel-prize-laureates.csv'''
-        self.assertTrue(os.path.exists("nobel-prize-laureates.csv"),
+        self.assertTrue(os.path.exists(AG+"nobel-prize-laureates.csv"),
                         "nobel-prize-laureates.csv does not exist!")
         out = check_output(["wc", "-l" ,"nobel-prize-laureates.csv"])
         num_lines = int(out[:-26]) 
@@ -47,12 +49,16 @@ class TestHelloWorld(unittest.TestCase):
         file_size = os.path.getsize('nobel-prize-laureates.csv')
         self.assertGreater(file_size, 100000, "nobel-prize-laureates.csv is too small")
         self.assertLess(file_size, 300000, "nobel-prize-laureates.csv is too large") 
-        file_content = open("nobel-prize-laureates.csv", "r").read()
+        file_content = open(AG+"nobel-prize-laureates.csv", "r").read()
         self.assertGreater(file_content.find("Pauling"), 0, "nobel-prize-laureates.csv does not contain 'Pauling'")
         self.assertLess(file_content.find("coordinates"), 0, "nobel-prize-laureates.csv contains 'coordinates' and shouldn't" ) 
       
     @weight(1)
     @number("4.0")
     def test_git_branch(self):
-        '''Can't check for git branches in the gradescope autograder environment.'''
-        
+        '''Submission completed '''
+        # gradescope strips out the git history, apparently using a github
+        # API endpoint that provides a snapshot of the repo as a tarball.
+        # It might be possible to grade these semiautomatically, but it would 
+        # entail cloning all the gh-classrooms repos and then running tests
+        # on them.
