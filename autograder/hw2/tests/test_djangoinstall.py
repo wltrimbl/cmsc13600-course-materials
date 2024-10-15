8e8458f0503dc859d0b815731010f1560523d65e
Will Trimble <nospam@nowhere.org>
2024-10-15 11:05:29 -0500

    Update tests

--- autograder/hw2/tests/test_djangoinstall.py
+++ autograder/hw2/tests/test_djangoinstall.py
@@ -18,20 +18,23 @@ class TestHelloWorld(unittest.TestCase):
     '''Test django install, contents of tables.txt'''
     @classmethod
     def setUpClass(self):
+        self.DEADSERVER = False
         print("starting server")
         p = subprocess.Popen(['python3', 'attendancechimp/manage.py', 'runserver'],
                              close_fds=True)
         sleep(2)
         # Make sure server is still running in background, or error
-#       self.assertEqual(p.returncode, None, "Error starting server"+ repr(p.communicate()))
-#         self.assertIsNone(p.returncode)
-        self.SERVER = p
+        if p.returncode is None:
+            self.SERVER = p
+        else: 
+           self.DEADSERVER = True
+           print(pp.communicate()) 
 
     @classmethod
     def tearDownClass(self):
         self.SERVER.terminate()
 
-    @weight(1)
+    @weight(0)
     @number("1.0")
     def test_exist_tables(self): # Should this be tables.txt or .csv
         '''Test for tables.txt file'''
@@ -39,13 +42,31 @@ class TestHelloWorld(unittest.TestCase):
                         os.path.exists("tables.txt"),
                         "tables.txt not found")
 
+    @weight(1)
+    @number("1.5")
+    def test_content_tables(self): # Should this be tables.txt or .csv
+        '''Test content of tables.txt file'''
+        tables = ""
+        if os.path.exists("attendancechimp/tables.txt"):
+             tables = "attendancechimp/tables.txt"
+        if os.path.exists("tables.txt"):
+             tables = "tables.txt"
+        self.assertTrue(tables != "", "tables.txt not found")
+        content = open(tables, "r").read()
+        self.assertTrue("auth_permission" in content,
+                        "tables.txt does not contain expected table names")
+        self.assertTrue("auth_user" in content,
+                        "tables.txt does not contain expected table names")
+        self.assertTrue("django_migrations" in content,
+                        "tables.txt does not contain expected table names")
+
 
     @weight(1)
     @number("2.0")
     def test_runserver_app_time(self):
         '''Test the time view runs ok'''
-        self.setUp()
-        self.assertTrue(1)
+        if self.DEADSERVER:
+            self.assertFalse(True, "Django server didn't start")
         q = check_output(["curl", "--no-progress-meter", "http://127.0.0.1:8000/app/time"])
         print(q)
 
@@ -53,35 +74,36 @@ class TestHelloWorld(unittest.TestCase):
     @number("3.0")
     def test_time_hour(self):
         '''Test that the hour of the time view is correct'''
-        self.setUp()
-        self.assertTrue(1)
+        if self.DEADSERVER:
+            self.assertFalse(True, "Django server didn't start")
         CDT = zoneinfo.ZoneInfo("America/Chicago")
         now = datetime.now().astimezone(CDT)
         timestr = now.strftime("%H:%M")
         response = requests.get('http://127.0.0.1:8000/app/time')
         print(response.text)
-        self.assertIn(timestr.split(":")[0], response.text.split(":")[0])
+        self.assertTrue(":" in response.text)
+        self.assertEqual(timestr.split(":")[0], response.text.split(":")[0])
 
     @weight(1)
     @number("4.0")
     def test_time_minutes(self):
         '''Test that the minutes of the time view is correct'''
-        self.setUp()
-        sleep(2)
-        self.assertTrue(1)
+        if self.DEADSERVER:
+            self.assertFalse(True, "Django server didn't start")
         CDT = zoneinfo.ZoneInfo("America/Chicago")
         now = datetime.now().astimezone(CDT)
         timestr = now.strftime("%H:%M")
         response = requests.get('http://127.0.0.1:8000/app/time')
         print(response.text)
-        self.assertIn(timestr.split(":")[1], response.text.split(":")[1])
+        self.assertTrue(":" in response.text)
+        self.assertEqual(timestr.split(":")[1], response.text.split(":")[1])
 
     @weight(1)
     @number("5.0")
     def test_runserver_app_sum(self):
         '''Test the sum view runs ok'''
-        self.setUp()
-        self.assertTrue(1)
+        if self.DEADSERVER:
+            self.assertFalse(True, "Django server didn't start")
         q = check_output(["curl", "--no-progress-meter", "http://127.0.0.1:8000/app/sum"])
         print(q)
 
@@ -89,7 +111,8 @@ class TestHelloWorld(unittest.TestCase):
     @number("6.0")
     def test_sum_content(self):
         '''Test that the sum function returns the correct output 1+2'''
-        self.setUp()
+        if self.DEADSERVER:
+            self.assertFalse(True, "Django server didn't start")
         response = requests.get('http://127.0.0.1:8000/app/sum?n1=1&n2=2')
         print(response.text)
         self.assertIn('3', response.text)
@@ -98,7 +121,8 @@ class TestHelloWorld(unittest.TestCase):
     @number("7.0")
     def test_sum_content2(self):
         '''Test that the sum function returns the correct output 10.5+-6.2'''
-        self.setUp()
+        if self.DEADSERVER:
+            self.assertFalse(True, "Django server didn't start")
         response = requests.get('http://127.0.0.1:8000/app/sum?n1=10.5&n2=-6.2')
         print(response.text)
         self.assertIn('4.3', response.text)
@@ -107,7 +131,8 @@ class TestHelloWorld(unittest.TestCase):
     @number("8.0")
     def test_sum_content3(self):
         '''Test that the sum function returns the correct output 0.1+2.2'''
-        self.setUp()
+        if self.DEADSERVER:
+            self.assertFalse(True, "Django server didn't start")
         response = requests.get('http://127.0.0.1:8000/app/sum?n1=0.1&n2=2.2')
         print(response.text)
         self.assertIn('2.3', response.text)
