d2323a0173fb894b1616a5f323064b51c7fdcb2e
Will Trimble <nospam@nowhere.org>
2024-10-24 12:29:19 -0500

    Added Pillow, removed app/migrations

--- autograder/hw3/setup.sh
+++ autograder/hw3/setup.sh
@@ -3,5 +3,5 @@
 apt-get update && apt-get install -y python3-pip python3 file # python3-dev 
 
 # pytest is for debugging
-pip3 install gradescope-utils pylint pytest django
+pip3 install gradescope-utils pylint pytest django Pillow
 
