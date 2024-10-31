da51920cdb0344edfda8774f8704d863bbba31f4
Will Trimble <nospam@nowhere.org>
2024-10-30 19:57:37 -0500

    Added tests for login / login page contains email

--- autograder/hw4/tests/test_djangoapp.py
+++ autograder/hw4/tests/test_djangoapp.py
@@ -14,6 +14,8 @@ import zoneinfo
 from gradescope_utils.autograder_utils.decorators import weight, number
 import re
 import json
+import string
+import random
 
 if path.exists("/autograder"):
     AG = "/autograder"
@@ -39,6 +41,14 @@ class TestDjangoApp(unittest.TestCase):
         else:
             self.DEADSERVER = True
             print(pp.communicate())
+
+        self.user_dict =  {
+                "user_name": "Charlie",
+                "email": (random.choice(string.ascii_letters) +
+                         random.choice(string.ascii_letters) + "test@test.org"),
+                "is_student": "0",
+                "password": "Password123"
+                }
 
     @classmethod
     def tearDownClass(self):
@@ -94,11 +104,11 @@ class TestDjangoApp(unittest.TestCase):
         '''Checks the content of the new user form page (right fields, right endpoint)'''
         form_page_text = requests.get("http://localhost:8000/app/new").text
 
-        name_check = re.search("user_name", form_page_text, re.IGNORECASE) 
-        email_check = re.search("email", form_page_text) 
-        radio_check = re.search("radio", form_page_text, re.IGNORECASE) 
-        is_student_check = re.search("is_student", form_page_text) 
-        password_check = re.search("password", form_page_text) 
+        name_check = re.search("user_name", form_page_text, re.IGNORECASE)
+        email_check = re.search("email", form_page_text)
+        radio_check = re.search("radio", form_page_text, re.IGNORECASE)
+        is_student_check = re.search("is_student", form_page_text)
+        password_check = re.search("password", form_page_text)
         sign_up_check = re.search(r"Sign\s*UP", form_page_text, re.IGNORECASE)
         createuser_check = re.search("createUser", form_page_text)
         self.assertTrue(name_check, "Can't find 'user_name' field in app/new")
@@ -113,32 +123,32 @@ class TestDjangoApp(unittest.TestCase):
     def test_user_add_api(self):
         '''Checks that createUser endpoint responds with code 200
         when it should be successful'''
-        def post_fn_test():
-            user_dict = {
-                "user_name": "Charlie",
-                "email": "test@test.org",
-                "is_student": "0",
-                "password": "Password123"
-            }
-            response = requests.post("http://localhost:8000/app/createUser", data=user_dict)
-            if response.status_code != 200 :
-                self.assertEqual(response.status_code, 200, 
-                    "Wrong response code - {}".format(response.text) ) 
-        post_fn_test()
-#         with self.assertRaises(requests.exceptions.HTTPError):
-#            requests.exceptions.HTTPError
+        response = requests.post("http://localhost:8000/app/createUser",
+            data=self.user_dict)
+        if response.status_code != 200:
+            self.assertEqual(response.status_code, 200,
+                "Wrong response code - should pass - {}".format(response.text))
+
+    @weight(0.5)
+    @number("3.5")
+    def test_user_add_duplicate_email_api(self):
+        '''Checks that createUser endpoint responds with an error when
+        asked to add duplicate email.
+        '''
+        dup_user = self.user_dict.copy()
+        dup_user["user_name"] = "Different name"
+        response = requests.post("http://localhost:8000/app/createUser", data=dup_user)
+        if response.status_code == 200:
+            self.assertNotEqual(response.status_code, 200,
+                 "Wrong response code - should fail for duplicate email - {}".format(
+                 response.text))
 
     @weight(1)
     @number("4")
     def test_user_add_api_raises(self):
         '''Checks that createUser endpoint does not take GET'''
-        user_dict = {
-                "Name": "Charlie",
-                "email": "test@test.org",
-                "Student/Instructor": "Instructor",
-                "Password": "Password123"
-            }
-        response = requests.get("http://localhost:8000/app/createUser", json=user_dict)
+        response = requests.get("http://localhost:8000/app/createUser",
+            data=self.user_dict)  # data doesn't matter
         if response.status_code == 404:
             self.assertTrue(False, "GET to app/createUser returns HTTP 404 {}".format(
                 reseponse.text))
@@ -158,69 +168,29 @@ class TestDjangoApp(unittest.TestCase):
         self.assertIn("not logged", response.text.lower(),
                       "http://localhost:8000/ response does not contain phrase 'Not logged in'")
 
-
-"""
-    @weight(0)
-    @number("3.0")
-    def test_index_time(self):
-        '''Test that '''
-        if self.DEADSERVER:
-            self.assertFalse(True, "Django server didn't start")
-        response = requests.get('http://127.0.0.1:8000/')
-        now = datetime.now().astimezone(CDT)
-        timestr = now.strftime("%H:%M")
-        self.assertIn(timestr, response.text, "index.html does not contain{}".format(timestr))
-
-    @weight(0)
-    @number("4.0")
-    def test_time_minutes(self):
-        '''Test that '''
-        if self.DEADSERVER:
-            self.assertFalse(True, "Django server didn't start")
-        CDT = zoneinfo.ZoneInfo("America/Chicago")
-        now = datetime.now().astimezone(CDT)
-        timestr = now.strftime("%H:%M")
-        response = requests.get('http://127.0.0.1:8000/app/time')
-        print(response.text)
-#        self.assertTrue(":" in response.text)
-#        self.assertEqual(timestr.split(":")[1], response.text.split(":")[1])
-
     @weight(1)
-    @number("5.0")
-    def test_runserver_app_sum(self):
-        '''Test the '''
-        if self.DEADSERVER:
-            self.assertFalse(True, "Django server didn't start")
-        q = check_output(["curl", "--no-progress-meter", "http://127.0.0.1:8000/app/sum"])
-        print(q)
-
-    @weight(1)
-    @number("6.0")
-    def test_sum_content(self):
-        '''Test that '''
-        if self.DEADSERVER:
-            self.assertFalse(True, "Django server didn't start")
-        response = requests.get('http://127.0.0.1:8000/app/sum?n1=1&n2=2')
-        print(response.text)
-#        self.assertIn('3', response.text)
-
-    @weight(0)
-    @number("7.0")
-    def test_sum_content2(self):
-        '''Test that '''
-        if self.DEADSERVER:
-            self.assertFalse(True, "Django server didn't start")
-        response = requests.get('http://127.0.0.1:8000/app/sum?n1=10.5&n2=-6.2')
-        print(response.text)
-#        self.assertIn('4.3', response.text)
-
-    @weight(0)
-    @number("8.0")
-    def test_sum_content3(self):
-        '''Test that '''
-        if self.DEADSERVER:
-            self.assertFalse(True, "Django server didn't start")
-        response = requests.get('http://127.0.0.1:8000/app/sum?n1=0.1&n2=2.2')
-        print(response.text)
-#        self.assertIn('2.3', response.text)
-"""
+    @number("6")
+    def test_user_login(self):
+        '''Checks accounts/login page for login success, index page for username'''
+        user_dict = self.user_dict
+        session = requests.Session()
+        response0 = session.get("http://localhost:8000/accounts/login/")
+        csrf = re.search(r'csrfmiddlewaretoken" value="(.*?)"', response0.text)
+        if csrf:
+            csrfdata = csrf.groups()[0]
+        else:
+            raise ValueError("Can't find csrf token in accounts/login/ page")
+        logindata = {"username": user_dict["email"], "password": user_dict["password"],
+                "csrfmiddlewaretoken": csrfdata}
+        loginheaders = {"X-CSRFToken": csrfdata, "Referer":
+                "http://localhost:8000/accounts/login/"}
+        response1 = session.post("http://localhost:8000/accounts/login/", data=logindata,
+              headers=loginheaders)
+        if response1.ok and "sessionid" in response1.cookies:
+             print("Success!")
+        self.assertEqual(response1.status_code, 200,
+                        "Server returns error for http://localhost:8000/accounts/login/" +
+                        "Content:{}".format(response1.text))
+        response2 = session.get("http://localhost:8000/")
+        self.assertIn(user_dict["email"], response2.text,
+            "Can't find username (email) on index.html {}".format(response2.text))
