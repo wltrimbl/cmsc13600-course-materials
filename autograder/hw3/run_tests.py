bd03e71b63041e92a463c23e91f7606387b7f858
Will Trimble <wltrimbl@users.noreply.github.com>
2025-04-30 11:31:40 -0500

    Minor adjustments to hw3 autograder setup

--- autograder/hw3/run_tests.py
+++ autograder/hw3/run_tests.py
@@ -5,4 +5,5 @@ if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('tests')
     with open('/autograder/results/results.json', 'w') as f:
         JSONTestRunner(visibility='visible', 
-                       stdout_visibilty="visible", stream=f).run(suite)
+                       stdout_visibility="visible", 
+                       stream=f).run(suite)
