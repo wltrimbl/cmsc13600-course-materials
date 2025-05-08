160489328f6b3d87b4b4ce752ed59e51bf02defd
Will Trimble <wltrimbl@users.noreply.github.com>
2025-05-07 22:44:29 -0500

    Modified run_tests to be more verbose in case of autograder crash

--- autograder/hw5/run_tests.py
+++ autograder/hw5/run_tests.py
@@ -3,6 +3,19 @@ from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('tests')
-    with open('/autograder/results/results.json', 'w') as f:
-        JSONTestRunner(visibility='visible', 
+    try:
+        with open('/autograder/results/results.json', 'w') as f:
+            JSONTestRunner(visibility='visible',
                        stdout_visibility = "visible", stream=f).run(suite)
+    # In the case that the autograder crashes, preserve the autograder 
+    # standard output and make sure it is visible
+    except Exception as e:
+        with open('/autograder/results/results.json', 'w') as f:
+            import traceback
+            f.write('{\n')
+            f.write('"score": 0,\n')
+            f.write('"output": "Autograder crashed:\\n')
+            f.write(traceback.format_exc().replace('"', '\\"').replace('\n', '\\n'))
+            f.write('",\n')
+            f.write('"visibility": "visible"\n')
+            f.write('}\n')
