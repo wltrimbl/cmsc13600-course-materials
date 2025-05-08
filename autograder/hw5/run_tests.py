4869cb7b6dc0b5d0636394d3f2ebac8808cf2005
Will Trimble <wltrimbl@users.noreply.github.com>
2025-05-08 09:34:25 -0500

    Seemingly working, tests with output when everything fails

--- autograder/hw5/run_tests.py
+++ autograder/hw5/run_tests.py
@@ -1,25 +1,30 @@
 import unittest
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
+import traceback
 
 if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.discover('tests')
     try:
+        suite = unittest.defaultTestLoader.discover('tests')
         with open('/autograder/results/results.json', 'w') as f:
-            JSONTestRunner(visibility='visible',
+            JSONTestRunner(
+                       visibility='visible',
                        stdout_visibility = "visible", 
                        stream=f, 
                        failfast=True  # Stop after the first crash,
                        # because tracebacks from crashed autograder aren't helpful
                        ).run(suite)
     # In the case that the autograder crashes, preserve the autograder 
-    # standard output and make sure it is visible
+    # standard output and make sure it is visible; must be a list of dictionaries
     except Exception as e:
         with open('/autograder/results/results.json', 'w') as f:
-            import traceback
+            f.write('[\n') 
             f.write('{\n')
             f.write('"score": 0,\n')
             f.write('"output": "Autograder crashed:\\n')
             f.write(traceback.format_exc().replace('"', '\\"').replace('\n', '\\n'))
             f.write('",\n')
-            f.write('"visibility": "visible"\n')
+            f.write('"visibility": "visible",\n')
+            f.write('"stdout_visibility": "visible"\n')
             f.write('}\n')
+            f.write(']\n')                               
+
