2739633082923c66dfba3e00ae207e608cca6f3a
Will Trimble <wltrimbl@users.noreply.github.com>
2026-01-05 17:55:36 -0600

    updated tests, tight around 19 colums x 1014 rows

--- autograder/hw0/tests/test_hw0targets.py
+++ autograder/hw0/tests/test_hw0targets.py
@@ -7,32 +7,29 @@ import platform
 from gradescope_utils.autograder_utils.decorators import weight, number
 
 if platform.system() == "Darwin":
-    MD5 = "md5"
     AG = "./"
 else:
-    MD5 = "md5sum"
     AG = "/autograder/submission/"
 
 
 class TestHW0(unittest.TestCase):
     '''Test targets for hw0: names.txt, nobel-prize-laureates-clean.csv,
     unneeded_data.csv'''
-    def setUp(self):
-        pass
 
     @weight(1)
     @number("1.0")
-    def test_names_txt_exists(self):
-        '''Test names.txt exists and is long enough to contain a name.'''
-        NAMES = AG+"names.txt"
+    def test_books_txt_exists(self):
+        '''Test names.txt exists and is long enough to contain a book review.'''
+        NAMES = AG+"books.txt"
         self.assertTrue(os.path.exists(NAMES),
-                        "names.txt does not exist!")
+                        "books.txt does not exist!")
         file_size = os.path.getsize(NAMES)
-        file_contents = open(NAMES).read()
-        self.assertGreater(file_size, 5,
-                           "names.txt is not large enough to contain a name")
+        with open(NAMES) as f:
+            file_contents = f.read()
+        self.assertGreater(file_size, 30,
+                           "books.txt is not large enough to contain a book review")
         self.assertTrue(" " in file_contents,
-                        "names.txt does not contain spaces??")
+                        "books.txt does not contain spaces??")
 
     @weight(1)
     @number("2.0")
@@ -52,36 +49,41 @@ class TestHW0(unittest.TestCase):
     @weight(1)
     @number("3.0")
     def test_nobel(self):
-        '''Check size and length of nobel-prize-laureates.csv'''
+        '''Check size, length, and contents of nobel-prize-laureates.csv
+        looking for likely corruption'''
         NP = AG + "nobel-prize-laureates-clean.csv"
         self.assertTrue(os.path.exists(NP),
                         NP + " does not exist!")
         self.assertFalse(os.path.exists(AG + "nobel-prize-laureates.csv"),
                         "nobel-prize-laureates.csv exists!")
-        out = check_output(["wc", "-l", NP])
-        print("OUT", out)
-        num_lines = int(out.strip().split()[0])
-        self.assertGreater(num_lines, 1013, NP + " has too few lines")
-        self.assertLess(num_lines, 1016, NP + " has too many lines")
+        with open(NP, "rb") as f:
+           num_lines = sum(1 for _ in f)
+        print("num_lines", num_lines)
+        self.assertGreater(num_lines, 1012, NP + " has too few lines")
+        self.assertLess(num_lines, 1017, NP + " has too many lines")
         file_size = os.path.getsize(NP)
-        self.assertGreater(file_size, 100000,  NP + " is too small")
-        self.assertLess(file_size, 480000, NP + " is too large")
-        file_content = open(NP, "r").read()
+        self.assertGreater(file_size, 260000,  NP + " is too small")
+        self.assertLess(file_size, 275000, NP + " is too large")
+        with open(NP, "r") as f:
+            file_content = f.read()
         self.assertFalse("coordinates" in file_content, 
             NP + " contains string 'coordinates' (and it shouldn't)") 
-        self.assertGreater(file_content.find("Pauling"), 0,
+        self.assertGreater(file_content.count("Pauling"), 0,
             NP + " does not contain 'Pauling'")
-        self.assertLess(file_content.find("coordinates"), 0,
-            NP + " contains 'coordinates' and shouldn't")
+        self.assertGreater(file_content.count(";"), 19247,
+            NP + " does not contain enough semicolons to account for all the fields")
+        self.assertIn("62.77737840196957, 16.754284354522902", file_content,  
+            NP + " does not contain expected data from geo_point_2d column")
+        self.assertIn("geo_point_2d", file_content,  
+            NP + " does not contain expected geo_point_2d column header")
 
     @weight(1)
     @number("4.0")
-    def test_git_branch(self):
+    def test_submission_ok(self):
         '''Submission completed.  Free point.'''
         # gradescope strips out the git history, apparently using a github
         # API endpoint that provides a snapshot of the repo as a tarball.
         # It might be possible to grade these semiautomatically, but it would
         # entail cloning all the gh-classrooms repos and then running tests
         # on them.
-        self.assertFalse(os.path.exists(AG + "nobel-prize-laureates.csv"),
-                         "nobel-prize-laureates.csv is checked in!")
+        pass
