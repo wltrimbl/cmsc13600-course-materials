1a5d9e08c049e27b1c26201736d274447f43e2f2
Will Trimble <wltrimbl@users.noreply.github.com>
2026-01-23 13:36:51 -0600

    Added uncommondata project that doesn't do anything but reply to /hellounicorn

---
+++ examples/uncommondata/uncommondata/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+import datetime
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = '<html lang="en"><body>It is now %s.</body></html>' % now
+    return HttpResponse(html)
+
+def hellounicorn(request):
+    return HttpResponse("HELLO, Unicorn! THIS IS NOT A TEST.")
