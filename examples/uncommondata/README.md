c5d5c07515bca9609f4410b4071c52c8d29c8d61
Will Trimble <wltrimbl@users.noreply.github.com>
2026-01-23 13:39:59 -0600

    Added credit where credit is due for copied example code

---
+++ examples/uncommondata/README.md
@@ -0,0 +1,4 @@
+This is an almost empty django project created from the instructions on
+https://github.com/wltrimbl/cmsc13600-course-materials/blob/main/docs/HW-2-Django.md
+
+All it does is reply with text to http://localhost:8000/hellounicorn
--- examples/uncommondata/uncommondata/views.py
+++ examples/uncommondata/uncommondata/views.py
@@ -1,11 +1,14 @@
+# The first function is just copied from the django docs
+# on how to write views.py  : https://docs.djangoproject.com/en/6.0/topics/http/views/ 
+
 from django.http import HttpResponse
 import datetime
-
 
 def current_datetime(request):
     now = datetime.datetime.now()
     html = '<html lang="en"><body>It is now %s.</body></html>' % now
     return HttpResponse(html)
 
+# This is my business logic:
 def hellounicorn(request):
     return HttpResponse("HELLO, Unicorn! THIS IS NOT A TEST.")
